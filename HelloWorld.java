9d9484ee571591ded04a2db350a30ea205715f1c
suchitrah13 <suchitra.hapse@gmail.com>
2025-04-11 17:40:18 -0400

    Add HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package Selenium;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Printing Hello World
+		System.out.println("Hello World");
+		
+	}
+
+}
